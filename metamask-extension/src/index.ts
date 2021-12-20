cdd8fb1c2771607c8734e0b69b77dffe2432209d
Jakub Smékal <jakub.smekal@gmail.com>
2021-12-20 15:17:16 +0100

    added kernel extension

--- metamask-extension/src/index.ts
+++ metamask-extension/src/index.ts
@@ -3,32 +3,57 @@ import {
   JupyterFrontEndPlugin,
 } from '@jupyterlab/application';
 
-import { ICommandPalette } from '@jupyterlab/apputils';
-// import { ethers } from "ethers";
+import { ICommandPalette, InputDialog } from '@jupyterlab/apputils';
 
-declare global {
-  interface Window {
-      ethereum:any;
-  }
-}
+import { ILauncher } from '@jupyterlab/launcher';
 
-// const provider = new ethers.providers.Web3Provider(window.ethereum)
-// const signer = provider.getSigner()
-// signer.connect(provider)
-// console.log(signer)
-let accounts: any[] = [];
-async function getAccount() {
-  accounts = await window.ethereum.request({ method: 'eth_requestAccounts' });
-}
+import { ITranslator } from '@jupyterlab/translation';
+
+import { IRenderMimeRegistry } from '@jupyterlab/rendermime';
+import { ExamplePanel } from './panel';
+
 /**
- * Initialization data for the main menu example.
+ * The command IDs used by the console plugin.
  */
-const extension: JupyterFrontEndPlugin<void> = {
-  id: 'metamask_extension',
-  autoStart: true,
-  requires: [ICommandPalette],
-  activate: (app: JupyterFrontEnd, palette: ICommandPalette) => {
-    const { commands } = app;
+ namespace CommandIDs {
+  export const create = 'kernel-output:create';
+
+  export const execute = 'kernel-output:execute';
+}
+
+/**
+ * Activate the JupyterLab extension.
+ *
+ * @param app Jupyter Front End
+ * @param palette Jupyter Commands Palette
+ * @param rendermime Jupyter Render Mime Registry
+ * @param translator Jupyter Translator
+ * @param launcher [optional] Jupyter Launcher
+ */
+ function activate(
+  app: JupyterFrontEnd,
+  palette: ICommandPalette,
+  rendermime: IRenderMimeRegistry,
+  translator: ITranslator,
+  launcher: ILauncher | null
+): void {
+  const manager = app.serviceManager;
+  const { commands, shell } = app;
+  const category = 'Extension Examples';
+  const trans = translator.load('jupyterlab');
+
+  let panel: ExamplePanel;
+
+  /**
+   * Creates a example panel.
+   *
+   * @returns The panel
+   */
+  async function createPanel(): Promise<ExamplePanel> {
+    panel = new ExamplePanel(manager, rendermime, translator);
+    shell.add(panel, 'main');
+    return panel;
+  }
 
     // Add a command
     const command = 'connect_wallet';
@@ -72,15 +97,73 @@ const extension: JupyterFrontEndPlugin<void> = {
         );
       },
     });
-
-    // Add the command to the command palette
-    const category = 'Extension Examples';
-    palette.addItem({
-      command,
-      category,
-      args: { origin: 'from the palette' },
+    // add commands to registry
+    commands.addCommand(CommandIDs.create, {
+      label: trans.__('Open the Kernel Output Panel'),
+      caption: trans.__('Open the Kernel Output Panel'),
+      execute: createPanel,
     });
-  },
+
+    commands.addCommand(CommandIDs.execute, {
+      label: trans.__('Contact Kernel and Execute Code'),
+      caption: trans.__('Contact Kernel and Execute Code'),
+      execute: async () => {
+        // Create the panel if it does not exist
+        if (!panel) {
+          await createPanel();
+        }
+        // Prompt the user about the statement to be executed
+        const input = await InputDialog.getText({
+          title: trans.__('Code to execute'),
+          okLabel: trans.__('Execute'),
+          placeholder: trans.__('Statement to execute'),
+        });
+        // Execute the statement
+        if (input.button.accept) {
+          const code = input.value;
+          panel.execute(code);
+        }
+      },
+    });
+
+  // add items in command palette and menu
+  [CommandIDs.create, CommandIDs.execute].forEach((command) => {
+    palette.addItem({ command, category });
+  });
+
+  // Add launcher
+  if (launcher) {
+    launcher.add({
+      command: CommandIDs.create,
+      category: category,
+    });
+  }
+}
+
+
+declare global {
+  interface Window {
+      ethereum:any;
+  }
+}
+
+// const provider = new ethers.providers.Web3Provider(window.ethereum)
+// const signer = provider.getSigner()
+// signer.connect(provider)
+// console.log(signer)
+let accounts: any[] = [];
+async function getAccount() {
+  accounts = await window.ethereum.request({ method: 'eth_requestAccounts' });
+}
+/**
+ * Initialization data for the main menu example.
+ */
+const extension: JupyterFrontEndPlugin<void> = {
+  id: 'metamask_extension',
+  autoStart: true,
+  optional: [ILauncher],
+  requires: [ICommandPalette, IRenderMimeRegistry, ITranslator],
+  activate: activate
 };
 
 export default extension;
