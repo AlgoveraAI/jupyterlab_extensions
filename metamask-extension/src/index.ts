c4e3899a913b0c621992eedc439b06bf6caba2a7
Jakub Smékal <jakub.smekal@gmail.com>
2022-01-12 16:01:55 +0000

    Working transaction of OCEAN to test wallet

--- metamask-extension/src/index.ts
+++ metamask-extension/src/index.ts
@@ -11,8 +11,9 @@ import { ITranslator } from '@jupyterlab/translation';
 
 import { IRenderMimeRegistry } from '@jupyterlab/rendermime';
 import { ExamplePanel } from './panel';
+import * as  ethers from 'ethers';
 
-// import getPrivateKey from './address';
+import getPrivateKey from './address';
 
 import sendOcean from './transaction';
 
@@ -73,7 +74,7 @@ declare global {
       execute: (args: any) => {
         getAccount();
         console.log('accounts');
-        console.log(accounts);
+        console.log(account);
         // window.ethereum.request({ method: 'eth_requestAccounts' });
         console.log(
           `METAMASK EXTENSION LOADED.`
@@ -87,8 +88,11 @@ declare global {
       label: 'send transaction',
       caption: 'send transaction',
       execute: (args: any) => {
-        // const [privateKey, walletAddress] = getPrivateKey();
-        console.log(sendOcean);
+        const [privateKey, walletAddress] = getPrivateKey();
+        sendOcean(walletAddress);
+        console.log(privateKey)
+        console.log(walletAddress)
+
         // sendOcean(walletAddress);
         // console.log(privateKey)
         // window.ethereum
@@ -172,9 +176,15 @@ declare global {
 // const signer = provider.getSigner()
 // signer.connect(provider)
 // console.log(signer)
-let accounts: any[] = [];
+let account: any;
 async function getAccount() {
-  accounts = await window.ethereum.request({ method: 'eth_requestAccounts' });
+  const provider = new ethers.providers.Web3Provider(window.ethereum, "any");
+  // Prompt user for account connections
+  await provider.send("eth_requestAccounts", []);
+  const signer = provider.getSigner();
+  console.log("Account:", await signer.getAddress());
+  account = await signer.getAddress();
+  // accounts = await window.ethereum.request({ method: 'eth_requestAccounts' });
 }
 
 
