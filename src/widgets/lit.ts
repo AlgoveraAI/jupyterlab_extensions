63e09d482b6facf3791c90d957ca7f9bb2a48917
Jakub Smékal <jakub.smekal@gmail.com>
2022-02-25 20:15:30 +0000

    WIP: lit protocol integration

---
+++ src/widgets/lit.ts
@@ -0,0 +1,8 @@
+import { LitJsSdk } from "lit-js-sdk";
+
+const authSig = await LitJsSdk.checkAndSignAuthMessage({ chain: "ethereum" });
+
+const { zipBlob, encryptedString, symmetricKey } =
+  await LitJsSdk.encryptFileAndZipWithMetadata(
+    prompt("Enter file path.", "./")
+  );
