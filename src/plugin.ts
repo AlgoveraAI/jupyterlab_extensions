bab216cfbb251a7d9b6873364592a73e30648851
Jakub Smékal <jakub.smekal@gmail.com>
2022-03-06 18:43:11 +0000

    WIP: Estuary button

--- src/plugin.ts
+++ src/plugin.ts
@@ -126,6 +126,7 @@ function activate(
     caption: "save file",
     execute: (args: any) => {
       console.log("SAVE FILE EXTENSION LOADED");
+      upload();
     },
   });
 
@@ -194,6 +195,29 @@ async function saveFile() {
       prompt("Enter file path.", "./")
     );
   console.log(symmetricKey);
+}
+
+async function upload(e: any) {
+  e.persist();
+  console.log(e.target.files);
+
+  const formData = new FormData();
+  formData.append("data", e.target.files[0]);
+
+  // NOTE
+  // This example uses XMLHttpRequest() instead of fetch
+  // because we want to show progress. But you can use
+  // fetch in this example if you like.
+  const xhr = new XMLHttpRequest();
+
+  xhr.upload.onprogress = (event) => {
+    console.log(event.loaded);
+    console.log(event.total);
+  };
+
+  xhr.open("POST", "https://api.estuary.tech/content/add");
+  xhr.setRequestHeader("Authorization", "Bearer REPLACE_ME_WITH_API_KEY");
+  xhr.send(formData);
 }
 
 /**
