06b003f580392dd269ecacbbfb29971983b439b5
Jakub Smékal <jakub.smekal@gmail.com>
2022-07-14 08:53:35 +0200

    WIP: init ocean publish

---
+++ src/widgets/ocean_publish.ts
@@ -0,0 +1,7 @@
+export default async function publishToOcean() {
+  console.log("Publish to Ocean Initiated");
+  const algo_url = prompt(
+    "Enter URL of raw algorithm. This should be a URL to the raw text of your model.",
+    "15"
+  );
+}
