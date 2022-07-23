9ecbfb7b4066ba709166533c9f3ef50db78f6c44
Jakub Smékal <jakub.smekal@gmail.com>
2022-07-23 21:30:35 +0200

    WIP: added working js code for ocean publish

--- src/widgets/ocean_publish.ts
+++ src/widgets/ocean_publish.ts
@@ -1,177 +1,192 @@
-// import SHA256 from "crypto-js";
-// import * as o from "@oceanprotocol/lib";
+const { SHA256 } = require('crypto-js')
+const {
+      FixedRateExchange,
+      getHash,
+      Nft,
+      NftFactory,
+      ProviderInstance,
+      ZERO_ADDRESS
+} = require('@oceanprotocol/lib')
 
-// let config: Config;
-// let aquarius: Aquarius;
-// let providerUrl: any;
-// let publisherAccount: string;
-// let consumerAccount: string;
-// let stakerAccount: string;
-// let addresses: any;
-// let poolNftAddress: string;
-// let poolDatatokenAddress: string;
-// let poolAddress: string;
-// let freNftAddress: string;
-// let freDatatokenAddress: string;
-// let freAddress: string;
-// let freId: string;
-// let dispenserNftAddress: string;
-// let dispenserDatatokenAddress: string;
-// let dispenserAddress: string;
+const Web3 = require('web3')
+const fs = require('fs')
+const homedir = require('os')
 
-// export default async function publishToOcean() {
-//   console.log("Publish to Ocean Initiated");
-//   const algo_url = prompt(
-//     "Enter URL of raw algorithm. This should be a URL to the raw text of your model.",
-//     "15"
-//   );
-//   const algo_description = prompt(
-//     "Enter description of algorithm.",
-//     "e.g. 'A computer vision model.'"
-//   );
+export default async function publish(asset_url: string) {
+    // 0. SETUP
+    let config
+    let providerUrl
+    let publisherAccount
+    let addresses
+    let freNftAddress
+    let freDatatokenAddress
+    let freAddress
+    let freId
 
-//   const author = prompt(
-//     "Enter your name/name that will appear as author of the algorithm.",
-//     "Name Here"
-//   );
+    const FRE_NFT_NAME = 'Datatoken 2'
+    const FRE_NFT_SYMBOL = 'DT2'
 
-//   // Define Asset Metadata
-//   const ASSET_URL: Files = {
-//     datatokenAddress: "0x0",
-//     nftAddress: "0x0",
-//     files: [
-//       {
-//         type: "url",
-//         url: algo_url,
-//         method: "GET",
-//       },
-//     ],
-//   };
+    const ASSET_URL = {
+        datatokenAddress: '0x0',
+        nftAddress: '0x0',
+        files: [
+        {
+            type: 'url',
+            url: asset_url,
+            method: 'GET'
+        }
+        ]
+    }
 
-//   // Define MetaData
-//   const DDO = {
-//     "@context": ["https://w3id.org/did/v1"],
-//     id: "",
-//     version: "4.1.0",
-//     chainId: 4,
-//     nftAddress: "0x0",
-//     metadata: {
-//       created: "2021-12-20T14:35:20Z",
-//       updated: "2021-12-20T14:35:20Z",
-//       type: "dataset",
-//       name: "dataset-name",
-//       description: algo_description,
-//       author: author,
-//       license: "MIT",
-//     },
-//     services: [
-//       {
-//         id: "testFakeId",
-//         type: "access",
-//         files: "",
-//         datatokenAddress: "0x0",
-//         serviceEndpoint: "https://v4.provider.rinkeby.oceanprotocol.com",
-//         timeout: 0,
-//       },
-//     ],
-//   };
+    const DDO = {
+        '@context': ['https://w3id.org/did/v1'],
+        id: '',
+        version: '4.1.0',
+        chainId: 4,
+        nftAddress: '0x0',
+        metadata: {
+        created: '2021-12-20T14:35:20Z',
+        updated: '2021-12-20T14:35:20Z',
+        type: 'dataset',
+        name: 'dataset-name',
+        description: 'Ocean protocol test dataset description',
+        author: 'oceanprotocol-team',
+        license: 'MIT'
+        },
+        services: [
+        {
+            id: 'testFakeId',
+            type: 'access',
+            files: '',
+            datatokenAddress: '0x0',
+            serviceEndpoint: 'https://v4.provider.rinkeby.oceanprotocol.com',
+            timeout: 0
+        }
+        ]
+    }
 
-//   config = await getTestConfig(web3);
-//   aquarius = new Aquarius(config.metadataCacheUri);
-//   providerUrl = config.providerUri;
+    config = {
+    'network': 'rinkeby',
+    'nodeUri': 'https://rinkeby.infura.io/v3',
+    'BLOCK_CONFIRMATIONS': 0,
+    'metadataCacheUri' : 'https://v4.aquarius.oceanprotocol.com',
+    'providerUri' : 'https://v4.provider.rinkeby.oceanprotocol.com',
+    'downloads.path': 'consume-downloads',
+    }
+    providerUrl = config.providerUri
 
-//   console.log(`Aquarius URL: ${config.metadataCacheUri}`);
-//   console.log(`Provider URL: ${providerUrl}`);
+    console.log(`Aquarius URL: ${config.metadataCacheUri}`)
+    console.log(`Provider URL: ${providerUrl}`)
 
-//   //   Publish Data NFT and a Datatoken with a fixed rate exchange
+    // 1. ACCOUNTS & CONTRACTS
+    const web3 = new Web3(process.env.NODE_URI || 'http://127.0.0.1:8545') // to configure for rinkeby, see https://github.com/oceanprotocol/ocean.js/blob/efa3839d10befdbc35e16e61c8e9bf310039970b/src/utils/ConfigHelper.ts
+    publisherAccount = web3.eth.accounts.privateKeyToAccount('0xef4b441145c1d0f3b4bc6d61d29f5c6e502359481152f869247c7a4244d45209')
 
-//   const factory = new NftFactory(addresses.ERC721Factory, web3);
+    console.log(`Publisher account address: ${publisherAccount.address}`)
+    
+    const getAddresses = () => {
+        const data = JSON.parse(
+          // eslint-disable-next-line security/detect-non-literal-fs-filename
+          fs.readFileSync(
+            process.env.ADDRESS_FILE ||
+              `${homedir}/.ocean/ocean-contracts/artifacts/address.json`,
+            'utf8'
+          )
+        )
+        return data.development
+    }
+    addresses = getAddresses()
 
-//   const nftParams: NftCreateData = {
-//     name: FRE_NFT_NAME,
-//     symbol: FRE_NFT_SYMBOL,
-//     templateIndex: 1,
-//     tokenURI: "",
-//     transferable: true,
-//     owner: publisherAccount,
-//   };
+    // 2. PUBLISH DATA NFT & DATATOKEN WITH FIXED RATE EXCHANGE
+    const factory = new NftFactory(addresses.ERC721Factory, web3)
 
-//   const erc20Params: Erc20CreateParams = {
-//     templateIndex: 1,
-//     cap: "100000",
-//     feeAmount: "0",
-//     paymentCollector: ZERO_ADDRESS,
-//     feeToken: ZERO_ADDRESS,
-//     minter: publisherAccount,
-//     mpFeeAddress: ZERO_ADDRESS,
-//   };
+    const nftParams = {
+        name: FRE_NFT_NAME,
+        symbol: FRE_NFT_SYMBOL,
+        templateIndex: 1,
+        tokenURI: '',
+        transferable: true,
+        owner: publisherAccount.address
+    }
+  
+    const erc20Params = {
+        templateIndex: 1,
+        cap: '100000',
+        feeAmount: '0',
+        paymentCollector: ZERO_ADDRESS,
+        feeToken: ZERO_ADDRESS,
+        minter: publisherAccount.address,
+        mpFeeAddress: ZERO_ADDRESS
+    }
 
-//   const freParams: FreCreationParams = {
-//     fixedRateAddress: addresses.FixedPrice,
-//     baseTokenAddress: addresses.Ocean,
-//     owner: publisherAccount,
-//     marketFeeCollector: publisherAccount,
-//     baseTokenDecimals: 18,
-//     datatokenDecimals: 18,
-//     fixedRate: "1",
-//     marketFee: "0.001",
-//     allowedConsumer: ZERO_ADDRESS,
-//     withMint: false,
-//   };
+    const freParams = {
+        fixedRateAddress: addresses.FixedPrice,
+        baseTokenAddress: addresses.Ocean,
+        owner: publisherAccount.address,
+        marketFeeCollector: publisherAccount.address,
+        baseTokenDecimals: 18,
+        datatokenDecimals: 18,
+        fixedRate: '1',
+        marketFee: '0.001',
+        allowedConsumer: ZERO_ADDRESS,
+        withMint: false
+    }
 
-//   const tx = await factory.createNftErc20WithFixedRate(
-//     publisherAccount,
-//     nftParams,
-//     erc20Params,
-//     freParams
-//   );
+    const tx = await factory.createNftErc20WithFixedRate(
+        publisherAccount.address,
+        nftParams,
+        erc20Params,
+        freParams
+    )
+    
+  
+    freNftAddress = tx.events.NFTCreated.returnValues[0]
+    freDatatokenAddress = tx.events.TokenCreated.returnValues[0]
+    freAddress = tx.events.NewFixedRate.returnValues.exchangeContract
+    freId = tx.events.NewFixedRate.returnValues.exchangeId
 
-//   freNftAddress = tx.events.NFTCreated.returnValues[0];
-//   freDatatokenAddress = tx.events.TokenCreated.returnValues[0];
-//   freAddress = tx.events.NewFixedRate.returnValues.exchangeContract;
-//   freId = tx.events.NewFixedRate.returnValues.exchangeId;
+    console.log(`Fixed rate exchange NFT address: ${freNftAddress}`)
+    console.log(`Fixed rate exchange Datatoken address: ${freDatatokenAddress}`)
+    console.log(`Fixed rate exchange address: ${freAddress}`)
+    console.log(`Fixed rate exchange Id: ${freId}`)
 
-//   console.log(`Fixed rate exchange NFT address: ${freNftAddress}`);
-//   console.log(`Fixed rate exchange Datatoken address: ${freDatatokenAddress}`);
-//   console.log(`Fixed rate exchange address: ${freAddress}`);
-//   console.log(`Fixed rate exchange Id: ${freId}`);
+    // 2.5 Set metadata in the fixed rate exchange NFT
+    const nft = new Nft(web3)
 
-//   const nft = new Nft(web3);
+    DDO.chainId = await web3.eth.getChainId()
+    DDO.id =
+      'did:op:' +
+      SHA256(web3.utils.toChecksumAddress(freNftAddress) + DDO.chainId.toString(10))
+    DDO.nftAddress = freNftAddress
 
-//   DDO.chainId = await web3.eth.getChainId();
-//   DDO.id =
-//     "did:op:" +
-//     SHA256(
-//       web3.utils.toChecksumAddress(freNftAddress) + DDO.chainId.toString(10)
-//     );
-//   DDO.nftAddress = freNftAddress;
+    ASSET_URL.datatokenAddress = freDatatokenAddress
+    ASSET_URL.nftAddress = freNftAddress
+    const encryptedFiles = await ProviderInstance.encrypt(ASSET_URL, providerUrl)
+    DDO.services[0].files = await encryptedFiles
+    DDO.services[0].datatokenAddress = freDatatokenAddress
 
-//   ASSET_URL.datatokenAddress = freDatatokenAddress;
-//   ASSET_URL.nftAddress = freNftAddress;
-//   const encryptedFiles = await ProviderInstance.encrypt(ASSET_URL, providerUrl);
-//   DDO.services[0].files = await encryptedFiles;
-//   DDO.services[0].datatokenAddress = freDatatokenAddress;
+    console.log(`DID: ${DDO.id}`)
 
-//   console.log(`DID: ${DDO.id}`);
+    const providerResponse = await ProviderInstance.encrypt(DDO, providerUrl)
+    const encryptedDDO = await providerResponse
+    const metadataHash = getHash(JSON.stringify(DDO))
+    await nft.setMetadata(
+      freNftAddress,
+      publisherAccount.address,
+      0,
+      providerUrl,
+      '',
+      '0x2',
+      encryptedDDO,
+      '0x' + metadataHash
+    )
 
-//   const providerResponse = await ProviderInstance.encrypt(DDO, providerUrl);
-//   const encryptedDDO = await providerResponse;
-//   const metadataHash = getHash(JSON.stringify(DDO));
-//   await nft.setMetadata(
-//     freNftAddress,
-//     publisherAccount,
-//     0,
-//     providerUrl,
-//     "",
-//     "0x2",
-//     encryptedDDO,
-//     "0x" + metadataHash
-//   );
+    const fixedRate = new FixedRateExchange(web3, freAddress)
+    const oceanAmount = await (
+      await fixedRate.calcBaseInGivenOutDT(freId, '1')
+    ).baseTokenAmount
 
-//   const fixedRate = new FixedRateExchange(web3, freAddress);
-//   const oceanAmount = await (
-//     await fixedRate.calcBaseInGivenOutDT(freId, "1")
-//   ).baseTokenAmount;
+    console.log(`Price of 1 ${FRE_NFT_SYMBOL} is ${oceanAmount} OCEAN`)
 
-//   console.log(`Price of 1 ${FRE_NFT_SYMBOL} is ${oceanAmount} OCEAN`);
-// }
+      
+}
