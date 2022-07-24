228473ebf310f13d0bf4c89e12d3571a29f3d1a0
Jakub Smékal <jakub.smekal@gmail.com>
2022-07-24 20:26:47 +0200

    WIP: include polyfill webpack

--- webpack.config.js
+++ webpack.config.js
@@ -1,9 +1,14 @@
 module.exports = {
   resolve: {
+    extensions: [".tsx", ".ts", ".js"],
     fallback: {
-      http: false,
-      https: false,
-      stream: false,
+      http: require.resolve("stream-http"),
+      https: require.resolve("https-browserify"),
+      stream: require.resolve("stream-browserify"),
+      //   http: false,
+      //   https: false,
+      //   stream: false,
+      //   browser: false
     },
   },
 };
