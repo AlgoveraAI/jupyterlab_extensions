02ae261a25d177e85bfeba1c1e179f18b4c6a851
Jakub Smékal <jakub.smekal@gmail.com>
2022-07-25 09:40:37 +0200

    WIP: added plugins, buffer, jupyter labextension install still not working

--- webpack.config.js
+++ webpack.config.js
@@ -1,11 +1,26 @@
+const webpack = require("webpack");
+
 module.exports = {
+  plugins: [
+    // Work around for Buffer is undefined:
+    // https://github.com/webpack/changelog-v5/issues/10
+    new webpack.ProvidePlugin({
+      Buffer: ["buffer", "Buffer"],
+    }),
+    new webpack.ProvidePlugin({
+      process: "process/browser",
+    }),
+  ],
   resolve: {
-    extensions: [".tsx", ".ts", ".js"],
+    extensions: [".tsx", ".ts", ".js", ".jsx", ".css"],
+    modules: ["node_modules"],
     fallback: {
       http: require.resolve("stream-http"),
       https: require.resolve("https-browserify"),
       stream: require.resolve("stream-browserify"),
-      assert: false,
+      buffer: require.resolve("buffer"),
+      assert: require.resolve("assert/"),
+      // "assert": false,
       // "browser": false,
     },
   },
