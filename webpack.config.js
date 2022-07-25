11ac51f2a131960b48e45b881cce99ad0c01de3b
Jakub Smékal <jakub.smekal@gmail.com>
2022-07-25 09:10:25 +0200

    WIP: debugging webpack rimraf error

--- webpack.config.js
+++ webpack.config.js
@@ -2,14 +2,11 @@ module.exports = {
   resolve: {
     extensions: [".tsx", ".ts", ".js"],
     fallback: {
-      // http: require.resolve("stream-http"),
-      // https: require.resolve("https-browserify"),
-      // stream: require.resolve("stream-browserify"),
+      http: require.resolve("stream-http"),
+      https: require.resolve("https-browserify"),
+      stream: require.resolve("stream-browserify"),
       assert: false,
-      http: false,
-      https: false,
-      stream: false,
-      browser: false,
+      // "browser": false,
     },
   },
 };
