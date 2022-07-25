2725430e763d6b60c6706b2884cedd3f37860d41
Jakub Smékal <jakub.smekal@gmail.com>
2022-07-25 07:56:00 +0200

    WIP: jlpm run build is without warnings

--- webpack.config.js
+++ webpack.config.js
@@ -2,13 +2,14 @@ module.exports = {
   resolve: {
     extensions: [".tsx", ".ts", ".js"],
     fallback: {
-      http: require.resolve("stream-http"),
-      https: require.resolve("https-browserify"),
-      stream: require.resolve("stream-browserify"),
-      //   http: false,
-      //   https: false,
-      //   stream: false,
-      //   browser: false
+      // http: require.resolve("stream-http"),
+      // https: require.resolve("https-browserify"),
+      // stream: require.resolve("stream-browserify"),
+      assert: false,
+      http: false,
+      https: false,
+      stream: false,
+      browser: false,
     },
   },
 };
